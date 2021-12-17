600163f96f12cdfdf992ac69ff8479d17298ff00
Luke Woods <lukedwoods@gmail.com>
2021-12-17 13:36:21 +0000

    cleanup old files

--- app.py
+++ app.py
@@ -1,55 +1,34 @@
 import streamlit as st
-import pandas as pd
-import altair as alt
-from collections import namedtuple
-import math
 
-import os
+# ------------------------------------------------------
+st.title('Counter Example')
+# ------------------------------------------------------
 
-from auth import *
-from config import *
+if 'count' not in st.session_state:
+    st.session_state.count = 0
 
+increment = st.button('Increment')
+if increment:
+    st.session_state.count += 1
 
-def main(user_id, user_email):
-    st.write(f"You're logged in as {user_email}")
+decrement = st.button('Decrement')
+if decrement:
+    st.session_state.count -= 1
 
-    if user_email.split('@')[1] == domain:
-        st.success('Login Successful')
-        st.title('Secret Algorithm 🤫 ')
-
-        # Altair showcase
-        with st.echo(code_location='below'):
-            # Secret Algorithm
-
-            total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-            num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-            Point = namedtuple('Point', 'x y')
-            data = []
-            points_per_turn = total_points / num_turns
-            for curr_point_num in range(total_points):
-                curr_turn, i = divmod(curr_point_num, points_per_turn)
-                angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-                radius = curr_point_num / total_points
-                x = radius * math.cos(angle)
-                y = radius * math.sin(angle)
-                data.append(Point(x, y))
-            st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-                .mark_circle(color='#0068c9', opacity=0.5)
-                .encode(x='x:Q', y='y:Q'))
-
-    else:
-        st.error('Login Rejected')
-        st.text(f'No secrets for you, {user_email}.')
+st.write(st.session_state.count)
 
 
+# ------------------------------------------------------
+st.title('Mirrored Widgets using Session State')
+# ------------------------------------------------------
+# https://blog.streamlit.io/session-state-for-streamlit/
 
+def update_first():
+    st.session_state.second = st.session_state.first
 
-if __name__ == '__main__':
-    st.set_page_config(
-    page_title="Internal App",     # String or None. Strings get appended with "• Streamlit". 
-    page_icon="🦄",                # String, anything supported by st.image, or None.
-    layout="centered",             # Can be "centered" or "wide". In the future also "dashboard", etc.
-    initial_sidebar_state="auto")  # Can be "auto", "expanded", "collapsed"
+def update_second():
+    st.session_state.first = st.session_state.second
 
-    # Initialise OAuth login
-    google_oauth()
+st.text_input(label='Textbox 1', key='first', on_change=update_first)
+st.text_input(label='Textbox 2', key='second', on_change=update_second)
+
