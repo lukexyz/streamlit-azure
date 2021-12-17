5364a13b779e5ca1071a4a1ef4cd87be38ce4791
Luke Woods <lukedwoods@gmail.com>
2021-12-17 16:08:45 +0000

    fix circular import due to streamlit filenames

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
-from flask import Flask
-app = Flask(__name__)
+import app as st
 
-@app.route("/")
-def home():
-    return "Hello this is your app running"
+# ------------------------------------------------------
+st.title('Counter Example')
+# ------------------------------------------------------
+
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+
+increment = st.button('Increment')
+if increment:
+    st.session_state.count += 1
+
+decrement = st.button('Decrement')
+if decrement:
+    st.session_state.count -= 1
+
+st.write(st.session_state.count)
+
+
+# ------------------------------------------------------
+st.title('Mirrored Widgets using Session State')
+# ------------------------------------------------------
+# https://blog.streamlit.io/session-state-for-streamlit/
+
+def update_first():
+    st.session_state.second = st.session_state.first
+
+def update_second():
+    st.session_state.first = st.session_state.second
+
+st.text_input(label='Textbox 1', key='first', on_change=update_first)
+st.text_input(label='Textbox 2', key='second', on_change=update_second)
+
