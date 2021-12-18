22308dfe67689e0972da788ba28806b89586db39
lukexyz <lukedwoods@gmail.com>
2021-12-18 11:41:08 +0000

    temp change back to flask

---
+++ streamlit_app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+# ------------------------------------------------------
+st.title('Counter Example')
+# ------------------------------------------------------
+
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+
+increment = st.button('Increment')
+if increment:
+    st.session_state.count += 1
+
+decrement = st.button('Decrement')
+if decrement:
+    st.session_state.count -= 1
+
+st.write(st.session_state.count)
+
+
+# ------------------------------------------------------
+st.title('Mirrored Widgets using Session State')
+# ------------------------------------------------------
+# https://blog.streamlit.io/session-state-for-streamlit/
+
+def update_first():
+    st.session_state.second = st.session_state.first
+
+def update_second():
+    st.session_state.first = st.session_state.second
+
+st.text_input(label='Textbox 1', key='first', on_change=update_first)
+st.text_input(label='Textbox 2', key='second', on_change=update_second)
+
