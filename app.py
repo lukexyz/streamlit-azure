aad9820be063796a34a0ac8ba961f1c0c981a1f4
lukexyz <lukedwoods@gmail.com>
2021-12-18 16:17:58 +0000

    gh action push

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask
-app = Flask(__name__)
+import streamlit as st
+
+# ------------------------------------------------------
+st.title('Counter Example')
+# ------------------------------------------------------
+
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+
+increment = st.button('Increment')
+if increment:
+    st.session_state.count += 1
+
+decrement = st.button('Decrement')
+if decrement:
+    st.session_state.count -= 1
+
+st.write(st.session_state.count)
 
 
-@app.route("/")
-def home():
-    return "Hello, welcome to flaskcozom. Where all your dreams come true."
+# ------------------------------------------------------
+st.title('Mirrored Widgets using Session State')
+# ------------------------------------------------------
+# https://blog.streamlit.io/session-state-for-streamlit/
 
+def update_first():
+    st.session_state.second = st.session_state.first
 
-if __name__ == '__main__':
-    app.run(debug=True)
+def update_second():
+    st.session_state.first = st.session_state.second
+
+st.text_input(label='Textbox 1', key='first', on_change=update_first)
+st.text_input(label='Textbox 2', key='second', on_change=update_second)
+
